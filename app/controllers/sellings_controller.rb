e9e33b1644244936ff3f56d43dfd54d41419103b
Andre Sardinha <acsardinha@hotmail.com>
2022-11-22 12:31:37 +0000

    Selling and Wigs Routes Created

---
+++ app/controllers/sellings_controller.rb
@@ -0,0 +1,2 @@
+class SellingsController < ApplicationController
+end
---
+++ test/controllers/sellings_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SellingsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
