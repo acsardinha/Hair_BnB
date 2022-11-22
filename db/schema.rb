1a0919b5f1ec8f85fe0573bf324afc881f12be2b
Alexandre Ferreira <alex.ferreira9915@gmail.com>
2022-11-22 12:07:41 +0000

    fxed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_114738) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_120631) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sellings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "wig_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sellings_on_user_id"
+    t.index ["wig_id"], name: "index_sellings_on_wig_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -38,5 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_114738) do
     t.index ["user_id"], name: "index_wigs_on_user_id"
   end
 
+  add_foreign_key "sellings", "users"
+  add_foreign_key "sellings", "wigs"
   add_foreign_key "wigs", "users"
 end
