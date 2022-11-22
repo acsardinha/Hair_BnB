9cf18b0e8d58412323aaba420fa22f422e9e1d33
Andre Sardinha <acsardinha@hotmail.com>
2022-11-22 11:57:06 +0000

    Wigs Model Created and Migrated 2

--- db/migrate/20221122114738_create_wings.rb
+++ db/migrate/20221122114738_create_wings.rb
@@ -1,6 +1,6 @@
 class CreateWings < ActiveRecord::Migration[7.0]
   def change
-    create_table :wings do |t|
+    create_table :wigs do |t|
       t.string :type
       t.string :color
       t.string :name
