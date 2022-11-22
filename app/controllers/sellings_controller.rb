02a520c6515280227a56b098a2b12fd4142a4825
Andre Sardinha <acsardinha@hotmail.com>
2022-11-22 15:15:54 +0000

    Incomplete SELLINGS

--- app/controllers/sellings_controller.rb
+++ app/controllers/sellings_controller.rb
@@ -1,2 +1,20 @@
 class SellingsController < ApplicationController
+  # before_action :authenticate_user!
+
+  def new
+    @selling = Selling.new
+  end
+
+  def create
+    @selling = Selling.new(selling_params)
+    @selling.user = current_user
+    @selling.save
+    redirect_to wigs_path
+  end
+
+  private
+
+  def selling_params
+    params.require(:selling).peremit(:user_id, :wig_id)
+  end
 end
