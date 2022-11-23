b0aaf70520ddf43c26eeb75d730051bd42a0947b
Alexandre Ferreira <alex.ferreira9915@gmail.com>
2022-11-23 16:59:48 +0000

    trying to fix navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Wig.destroy_all
 
 require 'open-uri'
-
+user = User.create!(email: "test@test.com", password: "123456")
 3.times do
-  wig = Wig.new(wig_type: "Curly", color: "Brown", name: "A Wig", description: "My wig", price: 10)
+  wig = Wig.new(wig_type: "Curly", color: "Brown", name: "A Wig", description: "My wig", price: 10, user: user)
   file = URI.open('https://res.cloudinary.com/dxtsuiudu/image/upload/v1669215465/development/a1li5oedexkrxdi9fbcx7dal2ui1.jpg')
-  wig.photos.attach(io: file, filename: "", content_type: 'image/jpg')
+  wig.photo.attach(io: file, filename: "", content_type: 'image/jpg')
   wig.save!
 end
