e659f3fcdf6a45700214554dc7cc993392d8c8b0
Alexandre Ferreira <alex.ferreira9915@gmail.com>
2022-11-23 16:37:08 +0000

    stash

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'open-uri'
+
+3.times do
+  wig = Wig.new(wig_type: "Curly", color: "Brown", name: "A Wig", description: "My wig", price: 10)
+  file = URI.open('https://res.cloudinary.com/dxtsuiudu/image/upload/v1669215465/development/a1li5oedexkrxdi9fbcx7dal2ui1.jpg')
+  wig.photos.attach(io: file, filename: "", content_type: 'image/jpg')
+  wig.save!
+end
