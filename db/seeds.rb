beebfd9f031f6f0a827df0c81b530421e6537489
Andre Sardinha <acsardinha@hotmail.com>
2022-11-24 16:57:48 +0000

    Index style Half Way

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Selling.destroy_all
 Wig.destroy_all
 User.destroy_all
 
