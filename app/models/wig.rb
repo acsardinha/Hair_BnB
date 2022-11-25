308e2c46b6159e6daf3e413a266340bfb6808d54
Andre Sardinha <acsardinha@hotmail.com>
2022-11-25 17:03:32 +0000

    styling done

--- app/models/wig.rb
+++ app/models/wig.rb
@@ -2,10 +2,10 @@ class Wig < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
 
-  include PgSearch::Model
-  pg_search_scope :search_by_name,
-    against: [ :name ],
-    using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
-  }
+  # include PgSearch::Model
+  # pg_search_scope :search_by_name,
+  #   against: [ :name ],
+  #   using: {
+  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  # }
 end
